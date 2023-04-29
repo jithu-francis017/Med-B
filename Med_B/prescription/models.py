fe8aafb589e84b4b5d33a417b9a1000ebc476e55
Ramanand <ramuttenravitsr2002@gmail.com>
2023-04-29 21:22:03 +0530

    prescription_authentication updated

--- Med_B/prescription/models.py
+++ Med_B/prescription/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
 
 class Image(models.Model):
     caption = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
+
+
     def __str__(self):
         return self.caption
     
--- Med_B/prescription/views.py
+++ Med_B/prescription/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from .form import ImageForm
 from .models import Image
 
 
+@login_required
 def index(request):
 
     form = ImageForm(data=request.POST, files=request.FILES)
@@ -13,14 +14,17 @@ def index(request):
 
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
+            
 
-            form.save()
+            image = form.save(commit=False)
+            image.user = request.user
+            image.save()
             obj = form.instance
             return render(request, "prescription.html", {"obj": obj})
 
 
         else:
             form = ImageForm()
-    img = Image.objects.all()
+    img = Image.objects.filter(user=request.user)
     
     return render(request, "prescription.html",{"img": img, "form": form})
